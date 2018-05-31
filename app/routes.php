eff68ab717af2a629a9a6d752bbedab1a726e22b
Andy Groff <andrew.p.groff@gmail.com>
2018-05-31 22:36:35 +0000

    removing debug

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,8 @@ require "helpers.php";
 
 $uri = $_SERVER["REQUEST_URI"];
 
-echo $uri;
+//echo $uri;
+
 if($uri !== '/'){
     http_response_code(404);
     echo "404 not found."; // provide your own HTML for the error page
