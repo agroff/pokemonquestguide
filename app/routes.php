4c41da49dbd56ab63b14f471966a23ac039b2799
Andy Groff <andrew.p.groff@gmail.com>
2018-05-31 20:41:27 +0000

    testing

--- app/routes.php
+++ app/routes.php
@@ -4,6 +4,7 @@ require "helpers.php";
 
 $uri = $_SERVER["REQUEST_URI"];
 
+echo $uri;
 if($uri !== '/'){
     http_response_code(404);
     echo "404 not found."; // provide your own HTML for the error page
